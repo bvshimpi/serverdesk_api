25185535efecff6c25d2b2a9daf8368f76ff3d06
bvshimpi <bhaveshs@WinjitTechnologiesPvtLtd.local>
2018-11-30 11:29:38 +0530

    test

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    port: 9886,
+    databaseHost: 'localhost',
+    databaseUser: 'root',
+    databasePassword: 'winjit@123',
+    databaseDatabaseName: 'serverdesk',
+    privateKey: '37LvPsm4vaBcd4CY',
+    frontEndHost: "http://localhost:4200",
+    emailAccountUserName: 'pavan.winjit@gmail.com',
+    emailAccountPassword: 'PavanOffice@1992',
+    FromEmail: 'pavan.winjit@gmail.com'
+  };
